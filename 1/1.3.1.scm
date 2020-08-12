5b3e9687e2960bf04a1be5bf8a31285541e34096
bspp1314 <linyuanpeng1314@gmail.com>
2020-08-12 15:08:57 +0800

    feat:过程做参数

---
+++ 1/1.3.1.scm
@@ -0,0 +1,54 @@
+
+(define (cube x)
+    (* x x x))
+
+
+
+(define (sum_intergers a b)
+    (if ( > a b)
+        0
+        + a (sum_intergers (+a 1) b )))
+
+
+(define (sum_cubes a b)
+    (if (> a b)
+        0
+        (+ (cube a)
+        (sum-cubes (+ a 1) b))))
+
+(define (pi_sum a b)
+    (if (> a b)
+            0
+            (+ (/ 1.0 (* a (+ a 2)))
+            (pi_sum (+ a 4) b))))
+
+
+(define (sum term a next b)
+        (if (> a b)
+            0
+        (+ (term a)
+        (sum term (next a) next b))))
+
+
+
+(define (inc n) (+ n 1))
+
+(define (sum_cubes2 a b)
+        (sum cube a inc b))
+
+(define (identity x) x)
+(define (sum_integers2 a b)
+    (sum identity a inc b))
+
+
+(define (pi_sum2 a b)
+    (define (pi_next b)
+        (+ b 4))
+    (define (pi_term a)
+       (/ 1.0 (* a (+ a 2))))
+    (sum pi_term a next b)    )
+
+
+(cube 10)
+(sum_cubes2 1 10)
+(sum_integers2 1 10)
---
+++ 1/1_30.scm
@@ -0,0 +1,58 @@
+;迭代的sum
+
+(define (sum term a next b)
+        (define (sum_iter a b sum)
+            (if (> a b)
+                sum
+                (sum_iter (next a) b (+ (term a) sum) )))
+    (sum_iter a b 0))
+
+
+
+(define (odd x)
+     (= (remainder n 2) 1))
+
+(define (sum term a next b)
+    (if (> a b)
+        0
+        (+ (term a)
+           (sum term (next a) next b))))
+
+(define (cube x)
+    (* x x x))
+
+(define (simpson f a b n)
+    ; 变量 h 的定义由条件 h=(b−a)/n 给出，将它翻译成表达式：
+   (define h 
+        (/ (- b a)n))
+
+    ;继续将下一个条件 yk=f(a+kh) 翻译成函数：
+    (define  (yk k)
+        (f (+ a (* k h))))
+
+
+    (define (factor k)
+        (cond ((or (= k 0) (= k n))
+                1)
+              ((odd? k)
+                4)
+              (else
+                2)))
+    
+    
+    (define (term k)
+        (* (factor k)
+           (yk k)))
+
+    (define (next k)
+        (+ k 1))
+
+    (if (not (even? n))
+        (error "n can't be odd")
+        (* (/ h 3)
+           (sum term 0.0 next n))))    
+
+
+(simpson cube 0 1 100)
+
+
